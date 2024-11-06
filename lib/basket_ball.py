f32590a70a2883208a5b21d5238b46281aa3c2d0
Kelvin Masambu <kevohmasambu@gmail.com>
2024-11-06 09:29:41 +0300

    done

--- lib/basket_ball.py
+++ lib/basket_ball.py
@@ -182,3 +182,149 @@ def game_dict():
             ]
         }
     }
+
+
+
+
+def num_points_per_game(player_name):
+    data = game_dict()
+
+    for player in data['home']['players']:
+        if player['name'] == player_name:
+            return player['points_per_game']
+
+    for player in data['away']['players']:
+        if player['name'] == player_name:
+            return player['points_per_game'] 
+
+    return 'Player not found'
+
+print(num_points_per_game('Kevin Lov'))
+
+
+
+
+def player_age(player_name):
+    data = game_dict()
+
+    for player in data['home']['players']:
+        if player['name'] == player_name:
+            return player['age']
+
+    for player in data['away']['players']:
+        if player['name'] == player_name:
+            return player['age']
+
+    return 'Player not found'
+
+print(player_age('Kevin Love'))
+
+
+
+
+def team_colors(team_name):
+    data = game_dict()
+
+    if data['home']['team_name'] == team_name:
+        return data['home']['colors']
+
+    if data['away']['team_name'] == team_name:
+        return data['away']['colors']
+
+print(team_colors('Washington Wizards'))
+
+
+
+
+def team_names():
+    data = game_dict()
+
+    return data['home']['team_name'], data['away']['team_name']
+
+print(team_names())
+
+
+
+
+def player_numbers(team_name):
+    data = game_dict()
+
+    numbers = []
+
+    if data['home']['team_name'] == team_name:
+        for player in data['home']['players']:
+            numbers.append(player['number'])
+
+    elif data['away']['team_name'] == team_name:
+        for player in data['away']['players']:
+            numbers.append(player['number'])
+
+    else:
+        return 'team not found' 
+
+    return numbers
+
+print(player_numbers('Washington Wizards'))
+
+
+
+
+def player_stats(player_name):
+    data = game_dict()
+
+    for player in data['home']['players']:
+        if player['name'] == player_name:
+            return player
+
+    for player in data['away']['players']:
+        if player['name'] == player_name:
+            return player
+
+    return f'{player_name} not found'
+
+print(player_stats('Rui Hachimura'))
+
+
+
+
+def average_rebounds_by_shoe_brand():
+    data = game_dict()
+
+    # Initialize lists for each shoe brand
+    nike_list = []
+    adidas_list = []
+    puma_list = []
+    jordan_list = []
+
+    # Collect rebound data for each shoe brand (both home and away teams)
+    for x in data['home']['players'] + data['away']['players']:
+        if x['shoe_brand'] == 'Nike':
+            nike_list.append(x['rebounds_per_game'])
+        elif x['shoe_brand'] == 'Adidas':
+            adidas_list.append(x['rebounds_per_game'])
+        elif x['shoe_brand'] == 'Puma':
+            puma_list.append(x['rebounds_per_game'])
+        elif x['shoe_brand'] == 'Jordan':
+            jordan_list.append(x['rebounds_per_game'])
+
+    # Calculate the averages for each brand
+    def calculate_avg(rebounds_list):
+        return sum(rebounds_list) / len(rebounds_list) if rebounds_list else 0
+
+    nike_avg = calculate_avg(nike_list)
+    adidas_avg = calculate_avg(adidas_list)
+    puma_avg = calculate_avg(puma_list)
+    jordan_avg = calculate_avg(jordan_list)
+
+    # Prepare the result
+    result = (
+        f'Nike: {nike_avg:.2f} rebounds\n'
+        f'Adidas: {adidas_avg:.2f} rebounds\n'
+        f'Puma: {puma_avg:.2f} rebounds\n'
+        f'Jordan: {jordan_avg:.2f} rebounds'
+    )
+
+    return result
+
+# Test the function
+print(average_rebounds_by_shoe_brand())
